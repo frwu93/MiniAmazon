4617dabaeaa949cd80235cd7a1825c051b3bec9e
Kevin FEng <kmf47@duke.edu>
2021-10-07 03:32:20 -0400

    Changed create.sql and load.sql for our design

--- db/load.sql
+++ db/load.sql
@@ -1,3 +1,9 @@
 \COPY Users FROM 'data/Users.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Buyers FROM 'data/Buyers.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Sellers FROM 'data/Sellers.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Products FROM 'data/Products.csv' WITH DELIMITER ',' NULL '' CSV
-\COPY Purchases FROM 'data/Purchases.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Sells FROM 'data/Sells.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Seller_Rating FROM 'data/Seller_Rating.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Cart FROM 'data/Cart.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Product_Rating FROM 'data/Product_Rating.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Order_History FROM 'data/Order_History.csv' WITH DELIMITER ',' NULL '' CSV
