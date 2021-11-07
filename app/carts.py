aa81e4ecc3ecf5333411c0852c6d87fb3f526bb2
Franklin Wu <fyw4@duke.edu>
2021-11-07 02:26:52 -0500

    adding update quantity to cart, just need to fix the html file

--- app/carts.py
+++ app/carts.py
@@ -4,6 +4,8 @@ import datetime
 
 from .models.cart import Cart
 from .models.product import Product
+from flask import render_template, redirect, url_for, flash, request
+
 
 
 from flask import Blueprint
@@ -16,13 +18,17 @@ bp = Blueprint('carts', __name__)
 def cart():
     print("go to cart")
     if current_user.is_authenticated:
-        cart_items = Cart.get_all_by_uid(current_user.id) #TODO: make this goddamn fcn
-        sellers = []
-        for item in cart_items:
-            sellers.append(Product.)
+        cart_items = Cart.get_cart_products_by_uid(current_user.id) #TODO: make this goddamn fcn
+        subtotal = Cart.get_subtotal(cart_items)
     else:
         cart_items = None
-    return render_template('cart.html', title='Cart', cart_items=cart_items)
+    return render_template('cart.html', title='Cart', cart_items=cart_items, subtotal = subtotal)
+
+@bp.route('/cart/changeQuantity/<int:buyer_id>-<int:product_id>-<int:quantity>')
+def changeQuantity(buyer_id, product_id, quantity):
+    print("Changing quantity of ", id, " to ", quantity)
+    Cart.change_quantity(buyer_id, product_id, quantity)
+    return redirect(url_for('carts.cart'))
 
 
     
--- app/models/cart.py
+++ app/models/cart.py
@@ -2,17 +2,47 @@ from flask import current_app as app
 
 
 class Cart:
-    def __init__(self, product_id, quantity):
+    def __init__(self, product_id, product_name, firstname, lastname, price, quantity):
         self.product_id = product_id
+        self.product_name = product_name
+        self.seller = firstname + " " + lastname
+        self.price = price
         self.quantity = quantity
+        self.total_cost = '{:.2f}'.format(price*quantity)
 
     @staticmethod
-    def get_all_by_uid(buyer_id):
+    def get_cart_products_by_uid(buyer_id):
         rows = app.db.execute('''
-SELECT product_id, quantity
-FROM Cart
-WHERE buyer_id = :buyer_id
-''',
+        SELECT Products.id, Products.name, Users.firstname, Users.lastname, price, Cart.quantity
+        FROM Cart, Products, Users
+        WHERE Cart.buyer_id = :buyer_id AND Cart.product_id = Products.id AND 
+        Users.id = seller_id
+        ''',
                               buyer_id=buyer_id)
         return [Cart(*row) for row in rows]
+
+
+    
+    @staticmethod
+    def get_subtotal(cart : list) -> float:
+        total = 0
+        for product in cart:
+            total += float(product.total_cost)
+        return '{:.2f}'.format(total)
+    
+    @staticmethod
+    def change_quantity(buyer_id, product_id, quantity):
+        try: 
+            app.db.execute('''
+    UPDATE Cart
+    SET quantity=:quantity
+    WHERE buyer_id = :buyer_id AND product_id = :product_id
+    ''',
+                                buyer_id=buyer_id,
+                                product_id = product_id,
+                                quantity=quantity)
+        except Exception as e:
+            print(e)
+            print(f"Could not change quantity of {product_id} to {quantity}")
+            return None
     
