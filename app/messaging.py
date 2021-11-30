e70c9f88a66ef4c404abe91c6ef0f83ef01a70de
Devon Sinha <ds490@duke.edu>
2021-11-30 01:53:37 -0500

    TEST!

---
+++ app/messaging.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from flask_login import current_user
+import datetime
+
+from .models.cart import Cart
+from .models.product import Product
+from flask import render_template, redirect, url_for, flash, request
+
+
+
+from flask import Blueprint
+bp = Blueprint('messaging', __name__)
+
+@bp.route('/messages', methods=['GET'])
+def messages():
+    print("go to messages")
+    return render_template('messaging.html', title='Message')
+
+@bp.route('/feedback.html', methods=['GET'])
+def feedback():
+    print("go to messageshtml")
+    return render_template('feedback.html', title='Feedback')
