b464f70034e3c532f78386889df250c4d5eb720e
Devon Sinha <ds490@duke.edu>
2021-12-07 14:01:41 -0500

    comments added

--- app/users.py
+++ app/users.py
@@ -161,7 +161,10 @@ def reviews():
     user = User.get(current_user.id)
     reviews =  Review.get_UserReviews(current_user.id)
     sellerReviews=Review.get_SellerReviews(current_user.id)
-    return render_template('profile_subpages/reviews.html', title='Reviews', user=user, reviews=reviews, sellerReviews=sellerReviews)
+    reviews4seller=Review.get_buyer_review_for_sellers(current_user.id)
+    print(reviews4seller)
+    print("hi")
+    return render_template('profile_subpages/reviews.html', title='Reviews', user=user, reviews=reviews, sellerReviews=sellerReviews, reviews4seller=reviews4seller)
 
 @bp.route('/settings', methods=['GET', 'POST'])
 def settings():
