dd871b2114f82f50adb6a74f3a3ac10106df61bb
Zachary Zheng <zz245@duke.edu>
2021-12-07 02:00:54 -0500

    fixed balance bug

--- app/orders.py
+++ app/orders.py
@@ -89,7 +89,7 @@ def checkout_success(coupon = None):
     Order.add_to_history(order_id, cart_items)
     User.updateBalanceWithdrawal(current_user.id, payment["total"])
     for item in cart_items:
-        individualCost = float(item.price) * float(item.quantity)
+        individualCost = calculate_payment([item], coupon)['total']
         curBalance = float(User.get(item.seller_id).balance) + individualCost
         User.updateBalanceDeposit(item.seller_id, item.price * item.quantity)
         User.add_sold(item.seller_id, individualCost, datetime.datetime.now(), curBalance, item.product_name, item.quantity)
